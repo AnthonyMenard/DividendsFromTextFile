f290e47d35adf05be53cc58108815a6e1a183432
AnthonyMenard <92995798+AnthonyMenard@users.noreply.github.com>
2023-01-06 11:37:27 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import re
+import datetime
 #Open the file in read mode and read its contents into a string
-with open('div_notepad.txt', "r") as oldfile:
+with open('dividends.txt', "r") as oldfile:
      text = oldfile.read()
 
 #Remove end of lines
 text = text.replace('\n', ' ')
 
-years = ['2022', '2023', '2024']
+#todo: update every year [current year, current +1, current year +2]
+#Find today's year
+today = datetime.date.today()
+year = today.year
+
+#create a list and convert it in string
+years = [year-1, year, year+1]
+years = [str(year) for year in years]
+
 #Annual ("A") needs to be followed by a year to be replaced, so we don't replace "A" in a Class A ETF/company
 frequency_years = [year + ' A' for year in years]
 
 #replace anything that is identical to the freq_years
 for freq_year in frequency_years:
     text = re.sub(r' {}'.format(freq_year), ' {}\n'.format(freq_year), text)
-print(text)
 
 frequency = ["Q", "M", "S"]
 #replace if it match the freq and if it is a single word
 for freq in frequency:
     #replace "Q", "M" and "S", if it is a perfect match (it won't change the word "EQUITY" even if we search for "Q")
     text = re.sub(r'\b {}\b'.format(freq), ' {}\n'.format(freq), text)
+
+#add end of line if "page number of number" fits
+text = re.sub(r'(page \d+\ of \d+)',' \\1\n', text)
 
 #Split the modified text into a list of lines
 lines = text.split("\n")
@@ -32,12 +43,15 @@ modified_lines_final = []
 
 #loop trought the lines
 for line in lines:
+
     #Skip the first character (a space)
     modified_line = line[1:]
+
     #add semi colons around a decimal (the dividend)
     modified_line = re.sub(r'(\d+\.\d+)', '; \\1 ;', modified_line)
+
     #Add the individual line into lines, which represents the text file
-    modified_lines.append(modified_line)
+    #modified_lines.append(modified_line) #useless
 
     #If the first character is a digit
     if modified_line[0].isdigit():
@@ -56,6 +70,7 @@ for line in lines:
 
 #Iterate over the list of lines
 for i in range(len(modified_lines_final)):
+
     #split into words
     words = modified_lines_final[i].split()
     #add a semi colon after the first word
@@ -63,13 +78,22 @@ for i in range(len(modified_lines_final)):
     #reconstruct the sentences
     modified_lines_final[i] = ' '.join(words)
 
+
 #Join the modified lines back into a single string
 modified_text = "\n".join(modified_lines_final)
 
 #add a coma to convert the dates and add a semi colon for all years
 for year in years:
-    modified_text = modified_text.replace(year, ', ' + year + ' ;')
+    modified_text = modified_text.replace(year,',' + year + ' ;')
+modified_text = modified_text.replace(' ; ', ';')
+
 
 #Output the final result in a new text file
-with open('div_notepad_cleaned.txt', "w") as f:
+with open('dividends_cleaned.txt', "w") as f:
     f.write(modified_text)
+
+
+
+
+
+
